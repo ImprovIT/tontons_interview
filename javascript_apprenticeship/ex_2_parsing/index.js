91395ef63830ae875daf1b737ba82a007da49bb9
Alexandre <alexandre.lim@hotmail.com>
2017-08-11 13:22:43 +0200

    Consume json data in various classes

--- javascript_apprenticeship/ex_2_parsing/index.js
+++ javascript_apprenticeship/ex_2_parsing/index.js
@@ -1,6 +1,6 @@
 import json from './data-json'
 
-//console.log(json);
+//console.log(json.data.customer);
 
 /*
 * Considering the following JSON : Creating a set of class which make working this the provided JSON Stream easy
@@ -11,38 +11,75 @@ import json from './data-json'
 *                   - And then npm start (or yarn start)
 */
 
+class Mission{
+    constructor(data){
+        this._name = data.name;
+        this._map = data.map;
+        this._date = data.date;
+        this._workforce = data.informations.homme2main;
+        this._handling = data.informations.manutention;
+        this._volume = data.informations.volume;
+        this._nbObjects = data.informations.objects;
+        this._objectList = data.informations.object_list;
+        this._distance = data.informations.distance;
+        this._duration = data.informations.durations.mission;
+        this._roadDuration = data.informations.durations.road;
+    }
+}
+
 class Customer {
-    constructor(){
-
+    constructor(data){ 
+        this._firstname = data.customer.firstname;
+        this._lastname = data.customer.lastname;
+        this._email = data.customer.email;
+        this._phonenumber = data.customer.phonenumber;
+        this._info = data.customer.info;
+        this._comments = data.informations.comments;
     }
 }
 
-class Destination {
-    constructor(){
-
+class Journey {
+    constructor(data){
+        this._fullAddress = data.formatted_address;
+        this._nameAdress = data.name;
+        this._thoroughfare = data.thoroughfare;
+        this._subThoroughfare = data.subThoroughfare;
+        this._locality = data.locality;
+        this._subLocality = data.subLocality;
+        this._postalCode = data.postal_code;
+        this._coutry = data.country;
+        this._latitude = data.coordinates.latitude;
+        this._longitude = data.coordinates.longitude;
+        this._map = data.map_url;
+        this._stairs = data.stairs;
+        this._lift = data.lift;
     }
 }
 
-class Origin {
-    constructor(){
-
+class Origin extends Journey {
+    constructor(data){
+        super(Object.assign(data.places.origin, data.informations.stairs.origin));
     }
 }
 
-class Information {
-    constructor(){
-
+class Destination extends Journey {
+    constructor(data){
+        super(Object.assign(data.places.destination, data.informations.stairs.destination));
     }
 }
 
-class Transaction {
-    constructor(){
-        
-    }
-}
+let mission = new Mission(json.data);
+let customer = new Customer(json.data);
+let origin = new Origin(json.data);
+let destination = new Destination(json.data)
 
+console.log(mission);
+console.log(customer);
+console.log(origin);
+console.log(destination);
 
 /*
 * Bonus Question : Once done, create a React component which will display this mission data
 * You can find inspiration on example.png
 */
+
